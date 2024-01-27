a5c832f0e2fc83ebf8449f5e7b762d8175e6f1b6
Muhammad Zeeshan <muhammadxzeeshan038@gmail.com>
2024-01-27 11:26:13 +0500

    server modified

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ import { app } from "./app.js";
 import { connectDb } from './data/database.js'
 
 connectDb();
-
+app.use((req , res , next)=>{
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+})
 app.listen(5000,()=>{
 
     console.log(`server is working on ${process.env.NODE_ENV}`);
